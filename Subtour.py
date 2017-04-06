dc90ec863f0842d273aa112d8296f90ca629e51a
Julio Ledesma <jl111@users.noreply.github.com>
2017-04-05 21:48:49 -0500

    Add files via upload
    Checks for connected components, checks whether the candidate solution is an array of either 0.0 or 1.0

--- Subtour.py
+++ Subtour.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import random
+from collections import deque
 
 def binary_to_edge(binaryArray, numNodes):
 	"""
@@ -34,7 +36,85 @@ def count_edge_nodes(edges, numNodes):
 		dictionary[num2] += 1
 	return dictionary
 
-def isSubtour(solution, graph):
+def bfs(g, startnode):
+    """
+    Perform a breadth-first search on g starting at node startnode.
+
+    Arguments:
+    g -- undirected graph
+    startnode - node in g to start the search from
+
+    Returns:
+    The distances from startnode to each node.
+    """
+    dist = {}
+
+    # Initialize distances and predecessors
+    for node in g:
+        dist[node] = float('inf')
+    dist[startnode] = 0
+
+    # Initialize the search queue
+    queue = deque([startnode])
+
+    # Loop until all connected nodes have been explored
+    while queue:
+        j = queue.popleft()
+        for h in g[j]:
+            if dist[h] == float('inf'):
+                dist[h] = dist[j] + 1
+                queue.append(h)
+    return dist
+
+def connected_components(g):
+    """
+    Find all connected components in g.
+
+    Arguments:
+    g -- undirected graph
+
+    Returns:
+    A list of sets where each set is all the nodes in
+    a connected component.
+    """
+    # Initially we have no components and all nodes remain to be
+    # explored.
+    components = []
+    remaining = set(g.keys())
+
+    while remaining:
+        # Randomly select a remaining node and find all nodes
+        # connected to that node
+        node = random.choice(list(remaining))
+        distances = bfs(g, node)
+        visited = set()
+        for i in remaining:
+            if distances[i] != float('inf'):
+                visited.add(i)
+        components.append(visited)
+
+        # Remove all nodes in this component from the remaining
+        # nodes
+        remaining -= visited
+    return components
+
+def make_Graph_from_Edges(edges):
+	"""
+	makes a dictionary Graph from an array of tuple edges
+	Input: edges -- array of tuples 
+	Output: dictionary of sets 
+	"""
+	graph = defaultdict(set)
+	for edges in edges:
+
+		node1 = edges[0]
+		node2 = edges[1]
+		graph[node1].add(node2)
+		graph[node2].add(node1)
+
+	return graph
+
+def isTour(solution, graph):
 	"""
 	determines whether a given solution is a binary 
 	represenation of edges in a cycle, and whether those 
@@ -45,21 +125,33 @@ def isSubtour(solution, graph):
 	"""
 	node_Degree = 2
 	numNodes = len(graph.keys())
+	#checks if solution is binary 
 	for edge in solution:
-		if edge not in [0,1]:
-			print "Entries in our solution must be binary"
+		if edge not in [0.0,1.0]:
+			print "Entries in our solution must be binary floats"
 			return False
-	edges = binary_to_edge(solution, numNodes)
 
+	edges = binary_to_edge(solution, numNodes)
 	if len(edges) != numNodes:
 		print "Number of edges must be the number of nodes for a cycle"
 		return False
+
 	nodeInstanceDictionary = count_edge_nodes(edges, numNodes)
 	for node in nodeInstanceDictionary.keys():
 		if nodeInstanceDictionary[node] != node_Degree:
 			print "a node's degree must be 2 in a cycle"
 			return False 
+
+	pathGraph = make_Graph_from_Edges(edges)
+	#print pathGraph
+	pathComponents = connected_components(pathGraph)
+	if len(pathComponents) > 1:
+		print "Tour has more than one connected components"
+		return False
+
 	return True
+#testcase with two connected components each of size 3
+x = [1.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,1.0,1.0]
 
 def makeGraph(numNodes):
 	"""makes undirected connected graph
@@ -72,6 +164,14 @@ def makeGraph(numNodes):
 			if i != j: 
 				graph[i].add(j)
 	return graph
+
+d = {1: set([2, 3]), 2: set([1, 3]), 3: set([1, 2]), 4: set([5, 6]), 5: set([4, 6]), 6: set([4, 5])}
+
+graph6 = makeGraph(6)
+print isTour(x,graph6)
+fail = [1.0,1.0,1.0]
+print isTour(fail, makeGraph(3))
+
 
 
 
